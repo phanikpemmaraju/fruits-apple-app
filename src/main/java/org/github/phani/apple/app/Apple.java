f3b7996282f991d9125093a3b47a20310532668d
Phani Pemmaraju <phani16@phanis-mbp.home>
2016-08-06 21:14:41 +0100

    Step 2 Version

--- src/main/java/org/github/phani/apple/app/Apple.java
+++ src/main/java/org/github/phani/apple/app/Apple.java
@@ -1,5 +1,94 @@
 package org.github.phani.apple.app;
 
 public class Apple {
+	
+	private String colour;
+	private Integer weight;
+	private Taste taste;
+	private Boolean isWorm;
 
+	public Apple(){		
+	}
+	
+	public Apple(String colour,Integer weight,Taste taste,Boolean isWorm){
+		this.colour = colour;
+		this.weight = weight;
+		this.taste = taste;
+		this.isWorm = isWorm;
+	}
+	
+	public String getColour() {
+		return colour;
+	}
+	public void setColour(String colour) {
+		this.colour = colour;
+	}
+	public Integer getWeight() {
+		return weight;
+	}
+	public void setWeight(Integer weight) {
+		this.weight = weight;
+	}
+	public Taste getTaste() {
+		return taste;
+	}
+	public void setTaste(Taste taste) {
+		this.taste = taste;
+	}
+	
+	public Boolean getIsWorm() {
+		return isWorm;
+	}
+
+	public void setIsWorm(Boolean isWorm) {
+		this.isWorm = isWorm;
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((colour == null) ? 0 : colour.hashCode());
+		result = prime * result + ((isWorm == null) ? 0 : isWorm.hashCode());
+		result = prime * result + ((taste == null) ? 0 : taste.hashCode());
+		result = prime * result + ((weight == null) ? 0 : weight.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Apple other = (Apple) obj;
+		if (colour == null) {
+			if (other.colour != null)
+				return false;
+		} else if (!colour.equals(other.colour))
+			return false;
+		if (isWorm == null) {
+			if (other.isWorm != null)
+				return false;
+		} else if (!isWorm.equals(other.isWorm))
+			return false;
+		if (taste != other.taste)
+			return false;
+		if (weight == null) {
+			if (other.weight != null)
+				return false;
+		} else if (!weight.equals(other.weight))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Apple [colour=" + colour + ", weight=" + weight + ", taste=" + taste + ", isWorm=" + isWorm + "]";
+	}
+	
+	
 }
--- src/test/java/org/github/phani/apple/app/AppleTest.java
+++ src/test/java/org/github/phani/apple/app/AppleTest.java
@@ -30,5 +30,44 @@ public class AppleTest {
 	public void GivenApple_ShouldReturnApple() {
 		assertNotNull(objectUnderTest);
 	}
+	
+	@Test
+	public void GivenAppleWithAttributes_ShouldReturnApple() {
+		objectUnderTest = new Apple("green",15,Taste.ONE,false);
+		assertNotNull(objectUnderTest);
+		assertEquals(objectUnderTest.getColour(),"green");
+		assertEquals(objectUnderTest.getWeight().intValue(),15);
+		assertEquals(objectUnderTest.getTaste().getValue(),1);
+		assertFalse(objectUnderTest.getIsWorm());
+	}
+	
+	@Test
+	public void GivenAppleWithNoColour_ShouldReturnApple() {
+		objectUnderTest = new Apple(null,15,Taste.ONE,false);
+		assertNotNull(objectUnderTest);
+		assertEquals(objectUnderTest.getColour(),null);
+	}
+	
+	@Test(expected=NullPointerException.class)
+	public void GivenAppleWithNoTaste_ShouldReturnException() {
+		objectUnderTest = new Apple("blue",15,null,false);
+		assertNotNull(objectUnderTest);
+		assertEquals(objectUnderTest.getTaste().getValue(),0);
+	}
+
+	@Test(expected=NullPointerException.class)
+	public void GivenAppleWithNoWeight_ShouldReturnException() {
+		objectUnderTest = new Apple("blue",null,Taste.THREE,false);
+		assertNotNull(objectUnderTest);
+		assertEquals(objectUnderTest.getWeight().intValue(),0);
+	}
+
+	@Test
+	public void GivenAppleWithWorm_ShouldReturnApple() {
+		objectUnderTest = new Apple("red",11,Taste.FOUR,true);
+		assertNotNull(objectUnderTest);
+		assertTrue(objectUnderTest.getIsWorm());
+	}
+
 
 }
